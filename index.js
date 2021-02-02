06dff20920662e1a6426d55dbdfb1bba69a46cf4
Jade Lynn Masker <jadelynn@masker.family>
2021-02-02 17:08:51 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
-// Your code here
+class Polygon {
+	constructor(sides) {
+		this.sides = sides;
+	}
+
+	get countSides() {
+		return this.sides.length;
+	}
+
+	get perimeter() {
+		return this.sides.reduce((acc, side) => {
+			return acc + side;
+		}, 0);
+	}
+}
+
+class Triangle extends Polygon {
+	get isValid() {
+		const largestSide = this.sides.reduce((largest, side) => {
+			return Math.max(largest, side);
+		}, 0);
+		if (largestSide - (this.perimeter - largestSide) < 0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+}
+
+class Square extends Polygon {
+	get isValid() {
+		if (this.sides.length !== 4) {
+			return false;
+		}
+		const side1 = this.sides[0];
+		return this.sides.reduce((isEqual, side) => {
+			if (isEqual) {
+				if (side1 === side) {
+					return true;
+				} else {
+					return false;
+				}
+			} else {
+				return false;
+			}
+		}, true);
+	}
+
+	get area() {
+		return this.sides[0]**2;
+	}
+}
